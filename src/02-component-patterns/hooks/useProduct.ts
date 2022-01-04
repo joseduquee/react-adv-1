a7adfffe2241b716936e78ef6160e7f026ed843a
Alexander Duque <joseduquee@gmail.com>
2022-01-04 13:07:30 +0100

    control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { OnChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -15,14 +15,8 @@ export const useProduct = ({
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
 
-  const isControlled = useRef(!!onChange);
-
   const increaseBy = (value: number): void => {
     
-    if (isControlled.current) {
-      return onChange!({ count: value, product });
-    }
-
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     //de esta forma no sera nunca menor que 0
--- src/02-component-patterns/hooks/useShoppingCar.ts
+++ src/02-component-patterns/hooks/useShoppingCar.ts
@@ -1,58 +1,32 @@
 import { useState } from "react";
 import { Product, ProductInCar } from "../interfaces/interfaces";
 
-
 export const useShoppingCar = () => {
+  const [shoppingCar, setShoppingCar] = useState<{
+    [key: string]: ProductInCar;
+  }>({});
 
-    const [shoppingCar, setShoppingCar] = useState<{
-        [key: string]: ProductInCar;
-      }>({});
+  const onProductCountChange = ({ count, product }: { count: number; product: Product }) => 
+  {
+    console.log({ count });
     
-      const onProductCountChange = ({
-        count,
-        product,
-      }: {
-        count: number;
-        product: Product;
-      }) => {
-        setShoppingCar((oldShoppingCar) => {
-          const productInCar: ProductInCar = oldShoppingCar[product.id] || {
-            ...product,
-            count: 0,
-          };
-    
-          if (Math.max(productInCar.count + count, 0) > 0) {
-            productInCar.count += count;
-            return {
-              ...oldShoppingCar,
-              [product.id]: productInCar,
-            };
-          }
-    
-          //Borrar el producto
-          const { [product.id]: toDelete, ...rest } = oldShoppingCar;
-          return {
-            ...rest,
-          };
-    
-          //   if (count === 0) {
-          //     const { [product.id]: toDelete, ...rest } = oldShoppingCar;
-          //     return {
-          //       ...rest,
-          //     };
-          //   }
-    
-          //   return {
-          //     ...oldShoppingCar,
-          //     [product.id]: { ...product, count },
-          //   };
-          // }
-        });
+    setShoppingCar((oldShoppingCar) => {
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCar;
+        return {
+          ...rest,
+        };
+      }
+
+      return {
+        ...oldShoppingCar,
+        [product.id]: { ...product, count },
       };
+    });
+  };
 
-    return {
-        shoppingCar,
-        onProductCountChange,
-    }
-
-}
+  return {
+    shoppingCar,
+    onProductCountChange,
+  };
+};
